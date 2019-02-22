4465c9640e54f9a8507547cc2f05e5521cdac54d
ostanioni <ostanioni@gmail.com>
2019-02-22 18:12:41 +0700

    Nav menu was created

---
+++ src/components/NavMenu.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+import { NavLink } from "react-router-dom"
+// import { withRouter } from 'react-router'
+/*
+const NavLinkStyled = styled(NavLink).attrs(
+)`
+  
+`
+*/
+const NavMenu = () => {  return (
+    <NavLink to="/about">About</NavLink>
+)}
+
+export default NavMenu
+
+
