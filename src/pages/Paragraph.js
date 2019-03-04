d9f524474b256a1dbe59127d52b5fbf60da9abc4
ostanioni <ostanioni@gmail.com>
2019-03-04 16:30:27 +0700

    component  created

---
+++ src/pages/Paragraph.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Article = (props) => {
+
+}
+
+class Chapter extends React.Component {
+
+  render(){
+    return(
+
+    )
+  }
+}
+
+<article>
+    <h2>Глава 2. Анализ</h2>
+    <section>
+        <h3>2.1 Реализация</h3>
+            <p>
+                Содержание 2.1 ....
+            </p>
+        <h3>Упражнения</h3>
+            <ol>
+                <li></li>
+                <!--
+                    ........
+                -->
+            </ol>
+    </section>
+    <section>
+        <h3>2.2 Анализ</h3>
+            <p>
+                    Содержание 2.1 ....
+            </p>
+        <h3>Упражнения</h3>
+            <ol>
+                <li></li>
+                <!--
+                    ........
+                -->
+            </ol>
+    </section>
+</article>
