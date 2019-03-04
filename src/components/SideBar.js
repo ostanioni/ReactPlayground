4b56a70be07707b2a6c34e61891e58d252013932
ostanioni <ostanioni@gmail.com>
2019-03-04 13:20:50 +0700

    styled component added

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { observer, inject } from "mobx-react"
-import styled, { createGlobalStyle, ThemeProvider } from 'styled-components'
+import styled from 'styled-components'
 
-export default class SideBar extends Component {
+
+let SideBarStyled = styled.nav.attrs(props=>{
+
+})`
+width: 100%;
+height: 100%;
+color: ${props=>props.theme.textColor};
+background-color: ${props=>props.theme.bgColor};
+`
+@inject('themesStore', 'settingsStore', 'langStore')
+@observer
+class SideBar extends Component {
   static propTypes = {
     prop: PropTypes
   }
 
   render() {
     return (
-      <nav>
-        
-      </nav>
+      <SideBarStyled>
+        <ul>
+          <li>HHHHHH</li>
+          <li>22222222</li>
+          <li>3333333</li>
+          <li>4444444</li>
+          <li>555555555</li>
+        </ul>
+      </SideBarStyled>
     )
   }
 }
+
+export default SideBar
