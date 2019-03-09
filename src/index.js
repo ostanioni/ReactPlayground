3d8f92d0f2c2208023b1f049dfd3d1ed48514167
ostanioni <ostanioni@gmail.com>
2019-03-09 20:59:32 +0700

    threejs example entered

--- src/index.js
+++ src/index.js
@@ -28,6 +28,34 @@ class App extends React.Component {
 ReactDOM.render( <App/>, document.getElementById('edf720cb-b61fe') )
 */
 import 'css/abc.css'
+import THREE from 'three'
+
+////////////////////////////////////////
+
+var scene = new THREE.Scene();
+var aspect = window.innerWidth / window.innerHeight;
+var camera = new THREE.PerspectiveCamera( 75, aspect, 0.1, 1000 );
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshNormalMaterial();
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+camera.position.z = 5;
+
+var render = function () {
+  requestAnimationFrame( render );
+  cube.rotation.x += 0.1;
+  cube.rotation.y += 0.1;
+  renderer.render( scene, camera );
+};
+
+render();
+
+
+///////////////////////////////////////
 console.clear()
 const worker = new Worker('./workers/sortQuick.js');
 
