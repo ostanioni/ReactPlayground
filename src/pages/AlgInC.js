15cd334d7d5c7a94077368518771c05d61605165
ostanioni <ostanioni@gmail.com>
2019-03-19 20:52:06 +0700

    v0.1.6

--- src/pages/AlgInC.js
+++ src/pages/AlgInC.js
@@ -3,23 +3,38 @@ import styled from 'styled-components'
 import { observer, inject } from "mobx-react"
 import axios from 'axios'
 
-
 @inject('settingsStore')
 @observer
 class AlgInC extends Component {
   constructor({match}){
     super()
     this.match = match
+    this.state = {
+      data: []
+    }
+  }
+  handleSuccess = (data)=>{
+    this.setState({'data': data})
   }
   render() {
     axios.get('/resources/Introduction.json')
-    .then(function (response) {
+    .then( (response)=>{
       // handle success
-      console.log(response.data);
+      this.handleSuccess(response.data);
     })
     return (
       <div>
-        <h1>Hello {this.match.params.username}!</h1>
+        {
+        this.state.data[this.props.settingsStore.lang].map( (el,idx)=>{
+              return (
+                <div key={el.label}>
+                  <h2>{el.label}</h2>
+                  <div dangerouslySetInnerHTML={{ __html: el.text }}></div>
+                </div>
+              )
+          
+        })
+      }
       </div>
     )
   }
