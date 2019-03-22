06fbb2bc99fe7b35be4cc76109943e73c4031643
ostanioni <ostanioni@gmail.com>
2019-03-22 15:49:32 +0700

    intermediate save

--- src/svg.js
+++ src/svg.js
@@ -5,25 +5,75 @@ import styled, { css, keyframes } from 'styled-components'
 
 import Snap from 'snapsvg';
 
+const SvgStyled = styled.svg.attrs(props=>({
+  id: props.id,
+  viewBox: props.viewBox,
+}))`
+border: 2px solid cyan;
+&.red {
+  // fill: ${props=>props.theme.bgColor};
+  // stroke: ${props=>props.theme.textColor};
+  fill: "#0f0";
+  stroke: "#000";
+}
+`
 
 const circle = [0,1,2,3,4,5,6,7,8,9]
 
 export default class Svg extends Component {
+  state = {
+    s: null,
+  }
   componentDidMount(){
-   // console.log( document.querySelector('#row0').childNodes[3] )
-   // 
-   var s = Snap("#svg");
-   console.log(Snap("#svg"))
-var bigCircle = s.circle(150, 150, 100);
-bigCircle.attr({
-  fill: "#bada55",
-  stroke: "#000",
-  strokeWidth: 5
-});
+
+    const s = Snap("#svg")
+    
+    for(let i=0; i<10; i++)
+      for(let j=0; j<10; j++){
+
+        const Circle = s.circle( 30 + 50*i, 35 + 100*j, 18)
+        .attr({
+                //fill: "#bada55",
+               // stroke: "#000",
+                strokeWidth: 3
+              })
+    
+        const Text = s.text( 23 + 50*i, 43 + 100*j, `${i}`)
+        .attr({
+                //stroke:"#f00",
+                //fill: "#f00",
+                font: "1.5rem sans-serif"
+              })
+        var Group = s.group(Circle, Text)
+        .attr({
+          // fill: "#00f",
+          // stroke: "#000"
+        }).addClass('red');
+        // console.log(Group)
+        Group.animate( {x: i*15, y: j*15}, i*j*150+500)
+        // Group.animate( { fill: "#00f", stroke: "#000"}, i*j*150+500)
+
+      }
+      
+      //fill:`rgb(${i*22},${j*22},${i})`},
+    //var arr = circle.map( (idx)=>{
+      //return s.text(idx*10, 50, "Snap");
+    //} )
+    // var t1 = s.text(50, 50, "Snap");
+    // var t2 = s.text(50, 50, ["S","n","a","p"]);
+    // Text path usage
+    // t1.attr({textpath: "M10,10L100,100"});
+    // or
+    // var pth = s.path("M10,10L100,100");
+    // t1.attr({textpath: pth});
+    // var discs = s.group(Circle, s.circle(200, 150, 70)); viewBox={"0 0 500 500"}
+
   }
   render() {
     return (
-      <svg id="svg" viewBox="0 0 500 500" style></svg>
+      <SvgStyled id="svg" viewBox="0 0 510 970">
+        
+      </SvgStyled>
     )
   }
 }
